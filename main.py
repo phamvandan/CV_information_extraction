5918cf328354a8c73789e1a5819a702c9454f42c
phamvandan <phamvandan.cntt2@gmail.com>
2019-12-18 08:18:05 +0700

    version 2 branch

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def run(model, input_path, debug_images_path):
     for image_path, filename in input_path_list:
         # convert: akansa3@uic.edu Aniket Kansara.jpg -> akansa3@uic.edu Aniket Kansara
         filename = ".".join(filename.split(".")[:-1])
-        # make new folder in the debug_images_path
+        # make new folder in the debug_images_path name akansa3@uic.edu Aniket Kansara
         folderName = filename
         debug_images_path = savePath
         debug_images_path = os.path.join(debug_images_path,folderName)
